9e428a71eb1ce6ba6a0e9f9280b0b685551f83a3
mohamed-mehdi-hyad <mehdihyad.mh@gmail.com>
2023-07-26 00:02:16 +0100

    adding script file

---
+++ sendMail.py
@@ -0,0 +1,30 @@
+# Import Packages For Email
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+
+#> Define The Function That Will Send The Email For Us
+def sendingMail():
+    email_sender = "mehdihyad.mh@gmail.com"
+    email_password = "f a e h l s u h l m h y v b a x"
+    email_receiver = 'mehdihyad.design@gmail.com'
+    subject = "Hello sir How are you Doing !"
+    body = "hey there man, you are great"
+    
+    emailMessage = EmailMessage()
+    emailMessage['From'] = email_sender
+    emailMessage['To'] = email_receiver
+    emailMessage['Subject'] = subject
+    emailMessage.set_content(body)
+
+    # Define Smtp connection
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465 , context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, emailMessage.as_string())
+        print("Message Sent Successfully!")
+
+
+#> Call The Function
+sendingMail()
